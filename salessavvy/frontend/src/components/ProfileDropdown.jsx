82cb0cab518c50fd8b58f8cbe6952981197e5b4d
Sulaim-Saadath <avoor2005@gmail.com>
2026-07-13 18:13:13 +0530

    Logout Module complete

--- salessavvy/frontend/src/components/ProfileDropdown.jsx
+++ salessavvy/frontend/src/components/ProfileDropdown.jsx
@@ -6,19 +6,41 @@ import "../App.css";
 export function ProfileDropdown({ username }) {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
+
   const toggleDropdown = () => setIsOpen(!isOpen);
-  const handleLogout = () => {
-    console.log("User logged out");
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch("http://localhost:9090/api/auth/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+
+      console.log(response.status);
+      console.log(await response.text());
+
+      if (response.ok) {
+        console.log("User successfully logged out");
+        navigate("/");
+      } else {
+        console.error("Failed to log out");
+      }
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
   };
+
   return (
     <div className="profile-dropdown">
       <button onClick={toggleDropdown}>
         <img src={useravatar} alt="User Avatar" />
         {username || "Guest"}
       </button>
+
       {isOpen && (
         <div className="dropdown-menu">
           <button>Profile</button>
+
           <button
             onClick={() => {
               navigate("/orders");
@@ -27,6 +49,7 @@ export function ProfileDropdown({ username }) {
           >
             Orders
           </button>
+
           <button onClick={handleLogout}>Logout</button>
         </div>
       )}
